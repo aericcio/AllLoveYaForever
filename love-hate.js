2b0c5e0beae105bfcac3823ef67898f95ac596dd
aericcio <aericcio@gmail.com>
2019-11-01 01:24:48 -0700

    yes

--- love-hate.js
+++ love-hate.js
@@ -2,16 +2,14 @@ $(document).ready(function(){
   $("#love-button").click(function(){
     $.ajax({url: "doyouloveme.html", success: function(result){
       $("#love-div").html(result);
-      $("#love-button").hide();
-      $("#hate-button").hide();
+      $("#button-holder").hide();
     }});
   });
   $("#hate-button").click(function(){
 
     $.ajax({url: "doyouhateme.html", success: function(result){
       $("#hate-div").html(result);
-      $("#love-button").hide();
-      $("#hate-button").hide();
+      $("#button-holder").hide();
     }});
   });
 });
