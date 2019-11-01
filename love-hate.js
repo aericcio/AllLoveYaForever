0f529ae74d322c2a3acc664f0dab926b2c094cc6
aericcio <aericcio@gmail.com>
2019-10-31 23:07:22 -0700

    button fix

--- love-hate.js
+++ love-hate.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-  $("love-button").click(function(){
+  $("#love-button").click(function(){
     $.ajax({url: "doyouloveme.html", success: function(result){
       $("#love-div").html(result);
     }});
   });
-  $("love-button").click(function(){
+  $("#hate-button").click(function(){
     $.ajax({url: "doyouhateme.html", success: function(result){
       $("#hate-div").html(result);
     }});
