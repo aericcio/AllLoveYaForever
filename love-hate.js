e393457e6131d8ab9d79924b46664bd7bf72c4e5
aericcio <aericcio@gmail.com>
2019-10-31 23:46:42 -0700

    nice

--- love-hate.js
+++ love-hate.js
@@ -2,11 +2,16 @@ $(document).ready(function(){
   $("#love-button").click(function(){
     $.ajax({url: "doyouloveme.html", success: function(result){
       $("#love-div").html(result);
+      $("#love-button").hide();
+
     }});
   });
   $("#hate-button").click(function(){
+
     $.ajax({url: "doyouhateme.html", success: function(result){
       $("#hate-div").html(result);
+      $("#love-hate").hide();
+
     }});
   });
 });
